bc822a040e4e6d03cff101ed45d7344d40ac6708
Oliver figueroa <oliverfigueroa101092@gmail.com>
2026-03-25 00:03:07 -0300

    fix: zona horaria ajustada a Argentina

--- config/app.php
+++ config/app.php
@@ -61,11 +61,11 @@ return [
     |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | is set to "America/Argentina/Buenos_Aires" by default as it is widely used.
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'America/Argentina/Buenos_Aires'),
 
     /*
     |--------------------------------------------------------------------------
