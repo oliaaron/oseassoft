1f4845cab563c1b157782339b408066d85f69283
Oliver figueroa <oliverfigueroa101092@gmail.com>
2026-05-08 00:44:55 -0300

    fix: grid 4 columnas compilado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -324,7 +324,9 @@
                     </p>
                 </div>
                 
-               <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+
+
 
 
                     
